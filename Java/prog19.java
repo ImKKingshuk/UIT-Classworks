a741d099e9b96c20b58248497894a55d877891d4
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-10-07 20:31:32 +0530

    updated

---
+++ Java/prog19.java
@@ -0,0 +1,12 @@
+//  Java Program to Show the Use of Return
+
+public class prog19 {
+    public static void main(String[] args) {
+        int result = add(5, 10);
+        System.out.println("Sum: " + result);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
