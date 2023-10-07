3737baf2a64339f11255bd4e91030a78f1dca161
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-10-07 20:15:25 +0530

    updated

---
+++ Java/prog11.java
@@ -0,0 +1,13 @@
+// Java Program to Show If-Else Selection Structure
+
+public class prog11 {
+    public static void main(String[] args) {
+        int number = 15;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
