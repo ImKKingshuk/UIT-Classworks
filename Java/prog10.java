0af2d093fac07ffb4a53c1b430144e9349e22f6b
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-10-07 20:14:41 +0530

    updated

---
+++ Java/prog10.java
@@ -0,0 +1,11 @@
+// Java Program to Show the Use of ?: Operator
+
+public class prog10 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int max = (a > b) ? a : b;
+        System.out.println("Maximum value: " + max);
+    }
+}
