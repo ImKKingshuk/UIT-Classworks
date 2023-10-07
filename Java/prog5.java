f703df58dbb85622df1e0fd6dc349f8fd49f83d7
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-10-07 20:09:42 +0530

    updated

---
+++ Java/prog5.java
@@ -0,0 +1,11 @@
+// Java Program to Show Boolean Variable Values
+
+public class prog5 {
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isCodingCool = false;
+
+        System.out.println("Is Java fun? " + isJavaFun);
+        System.out.println("Is coding cool? " + isCodingCool);
+    }
+}
