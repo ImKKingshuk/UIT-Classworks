e7527c0bbdfa90cd7a1b6b2a824a483b57fb3a1a
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-10-07 20:13:48 +0530

    updated

---
+++ Java/prog9.java
@@ -0,0 +1,14 @@
+// Java Program to Show Left and Right Shift Operations
+public class prog9 {
+    public static void main(String[] args) {
+        int number = 10;
+
+        // Left shift operation
+        int leftShiftedNumber = number << 1; // equivalent to number * 2
+        System.out.println("Left shifted by 1: " + leftShiftedNumber);
+
+        // Right shift operation
+        int rightShiftedNumber = number >> 1; // equivalent to number / 2
+        System.out.println("Right shifted by 1: " + rightShiftedNumber);
+    }
+}
