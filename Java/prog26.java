6d62084d88073014bef95d9340f6cd1a5a6b9888
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-10-07 20:58:45 +0530

    updated

---
+++ Java/prog26.java
@@ -0,0 +1,11 @@
+// Java Program to Show the Use of Static Method
+
+public class prog26 {
+    public static void main(String[] args) {
+        prog26.display();
+    }
+
+    public static void display() {
+        System.out.println("This is a static method.");
+    }
+}
