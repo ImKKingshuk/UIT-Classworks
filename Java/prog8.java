b4dae248d76691cf1f4de6be2e7957dd00167853
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-10-07 20:12:52 +0530

    updated

---
+++ Java/prog8.java
@@ -0,0 +1,12 @@
+// Java Program to Create and Print an Integer Array
+
+public class prog8 {
+    public static void main(String[] args) {
+        int[] intArray = {10, 20, 30, 40, 50};
+
+        System.out.print("Array values: ");
+        for (int i : intArray) {
+            System.out.print(i + " ");
+        }
+    }
+}
