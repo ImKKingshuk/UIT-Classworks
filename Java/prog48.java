f0e5351a1d920e8097e4aa48ee21d018872e5d34
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-10 14:02:13 +0530

    updated

---
+++ Java/prog48.java
@@ -0,0 +1,38 @@
+
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class prog48 {
+    public static void main(String[] args) {
+       
+        String sourceFilePath = "path/to/source/file.txt";
+        String destinationFilePath = "path/to/destination/copied_file.txt";
+
+    
+        try {
+            copyFile(sourceFilePath, destinationFilePath);
+            System.out.println("File copied successfully.");
+        } catch (IOException e) {
+            System.err.println("Error copying the file: " + e.getMessage());
+        }
+    }
+
+    private static void copyFile(String sourceFilePath, String destinationFilePath) throws IOException {
+       
+        try (FileInputStream fis = new FileInputStream(sourceFilePath);
+             FileOutputStream fos = new FileOutputStream(destinationFilePath)) {
+
+          
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                fos.write(buffer, 0, bytesRead);
+            }
+        }
+    }
+}
+
