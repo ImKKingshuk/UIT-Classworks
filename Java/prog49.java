2a8cf920fd36fdf0b1e9cab6f2093e594fe7f12a
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-10 14:04:15 +0530

    updated

--- Java/prog49.java
+++ Java/prog49.java
@@ -38,27 +38,27 @@ public class prog49<T> {
     }
 
     public static void main(String[] args) {
-        // Create a stack of integers
+    
         prog49<Integer> intStack = new prog49<>();
 
-        // Push some elements onto the stack
+      
         intStack.push(10);
         intStack.push(20);
         intStack.push(30);
 
-        // Pop elements from the stack and print them
+       
         while (!intStack.isEmpty()) {
             System.out.println("Popped: " + intStack.pop());
         }
 
-        // Create a stack of strings
+      
         prog49<String> stringStack = new prog49<>();
 
-        // Push some strings onto the stack
+       
         stringStack.push("Hello");
         stringStack.push("World");
 
-        // Pop strings from the stack and print them
+       
         while (!stringStack.isEmpty()) {
             System.out.println("Popped: " + stringStack.pop());
         }
