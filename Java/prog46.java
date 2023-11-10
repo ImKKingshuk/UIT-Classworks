1fbd44925f670aab6822b96ba8f81e0fecac7cf0
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-10 15:24:03 +0530

    updated

---
+++ Java/prog46.java
@@ -0,0 +1,4 @@
+// Show that when you are calculating the summation of numbers by two threads (assignment no 42) the time requirement is less than the time requirement by a single threaded program for doing the same task.
+public class prog46 {
+    
+}
---
+++ Java/progtest.java
@@ -0,0 +1,5 @@
+public class progtest {
+    public static void main(String[] args) {
+        System.out.println("meow meow  meow , meow");
+    } 
+}
