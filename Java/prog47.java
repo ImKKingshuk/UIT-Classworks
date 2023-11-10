486dcb404ed2c1699ce1744d03c332269145f9c1
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-10 13:59:14 +0530

    updated

---
+++ Java/prog47.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class prog47 {
+    public static void main(String[] args) {
+     
+        Scanner scanner = new Scanner(System.in);
+
+      
+        System.out.print("Enter something: ");
+
+    
+        String userInput = scanner.nextLine();
+
+      
+        System.out.println("You entered: " + userInput);
+
+      
+        scanner.close();
+    }
+}
