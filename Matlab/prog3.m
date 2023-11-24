f103a076619770e485e390718bdf36895983f0d3
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-25 04:16:22 +0530

    updated

---
+++ Matlab/prog3.m
@@ -0,0 +1,11 @@
+a = input('ENTER 1ST NUMBER : ');
+b = input('ENTER 2ND NUMBER : ');
+c = input('ENTER 3RD NUMBER : ');
+if(a>b && a>c)
+    disp(a);
+elseif(b>c && b>a)
+    disp(b);
+else
+    disp(c);
+    
+end
