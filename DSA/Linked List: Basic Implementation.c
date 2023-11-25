a26f77ac8bccec5acc85e10bc67aa57c2860ec78
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-25 07:44:38 +0530

    updated

--- DSA/Linked List: Basic Implementation.c
+++ DSA/Linked List: Basic Implementation.c
@@ -1,38 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define a Node structure
+// Define a Node structure for the linked list
 struct Node {
     int data;
     struct Node* next;
 };
 
-int main() {
-    // Create three nodes
-    struct Node* head = NULL;
-    struct Node* second = NULL;
-    struct Node* third = NULL;
-
-    head = (struct Node*)malloc(sizeof(struct Node));
-    second = (struct Node*)malloc(sizeof(struct Node));
-    third = (struct Node*)malloc(sizeof(struct Node));
-
-    // Assign data to the nodes
-    head->data = 1;
-    head->next = second;
-
-    second->data = 2;
-    second->next = third;
-
-    third->data = 3;
-    third->next = NULL;
-
-    // Display the linked list elements
+// Function to print the elements of the linked list
+void printList(struct Node* head) {
     struct Node* current = head;
     while (current != NULL) {
-        printf("%d ", current->data);
+        printf("%d -> ", current->data);
         current = current->next;
     }
+    printf("NULL\n");
+}
+
+// Function to insert a new node at the beginning of the linked list
+struct Node* insertAtBeginning(struct Node* head, int newData) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = newData;
+    newNode->next = head;
+    return newNode; // The new node becomes the new head
+}
+
+// Function to insert a new node at the end of the linked list
+struct Node* insertAtEnd(struct Node* head, int newData) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = newData;
+    newNode->next = NULL;
+
+    // If the list is empty, the new node becomes the head
+    if (head == NULL) {
+        return newNode;
+    }
+
+    // Traverse to the end of the list and add the new node
+    struct Node* current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    current->next = newNode;
+    return head; // Head remains unchanged
+}
+
+int main() {
+    // Creating an empty linked list
+    struct Node* head = NULL;
+
+    // Inserting elements into the linked list
+    head = insertAtBeginning(head, 3);
+    head = insertAtBeginning(head, 2);
+    head = insertAtBeginning(head, 1);
+
+    head = insertAtEnd(head, 4);
+    head = insertAtEnd(head, 5);
+
+    // Printing the linked list
+    printf("Linked List: ");
+    printList(head);
 
     return 0;
 }
