105d09df39e5d67180e64a495c06df68e98cd3ff
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-25 07:45:32 +0530

    updated

--- DSA/Binary Tree: Basic Implementation.c
+++ DSA/Binary Tree: Basic Implementation.c
@@ -1,41 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define a Node structure for a binary tree
+// Define a Node structure for the binary tree
 struct Node {
     int data;
     struct Node* left;
     struct Node* right;
 };
 
-// Function to create a new node
-struct Node* createNode(int value) {
+// Function to create a new node with a given data value
+struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = value;
+    newNode->data = data;
     newNode->left = NULL;
     newNode->right = NULL;
     return newNode;
 }
 
+// Function to perform an in-order traversal of the binary tree
+void inOrderTraversal(struct Node* root) {
+    if (root != NULL) {
+        inOrderTraversal(root->left);
+        printf("%d ", root->data);
+        inOrderTraversal(root->right);
+    }
+}
+
 int main() {
-    // Create a sample binary tree
+    // Creating a simple binary tree:
+    //        1
+    //       / \
+    //      2   3
+    //     / \
+    //    4   5
+
     struct Node* root = createNode(1);
     root->left = createNode(2);
     root->right = createNode(3);
     root->left->left = createNode(4);
     root->left->right = createNode(5);
 
-    // Display the binary tree using pre-order traversal
-    printf("Pre-order traversal: ");
-    // Define a recursive function for pre-order traversal
-    void preOrderTraversal(struct Node* node) {
-        if (node != NULL) {
-            printf("%d ", node->data);
-            preOrderTraversal(node->left);
-            preOrderTraversal(node->right);
-        }
-    }
-    preOrderTraversal(root);
+    // Performing in-order traversal
+    printf("In-order traversal: ");
+    inOrderTraversal(root);
+    printf("\n");
 
     return 0;
 }
