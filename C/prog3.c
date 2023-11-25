17702f43c41736a544e96db3432f9744218d1940
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-25 07:19:52 +0530

    updated

---
+++ C/prog3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Declare an array of integers
+    int numbers[] = {1, 2, 3, 4, 5};
+
+    // Display elements of the array
+    for (int i = 0; i < 5; ++i) {
+        printf("%d ", numbers[i]);
+    }
+
+    return 0;
+}
