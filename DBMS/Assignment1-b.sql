4424d7644296d2679b8a2447f20a2e7064992f8c
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-29 02:56:34 +0530

    updated

--- DBMS/Assignment1-b.sql
+++ DBMS/Assignment1-b.sql
@@ -7,7 +7,7 @@ values
 (2, 'Wonyoung', 'South Korea', 'CSE', 'IVE', '2021-9-1'),
 (3, 'Rose', 'New Zealand', 'CSE', 'BLɅϽKPIИK', '2021-9-1'),
 (4, 'Jennie', 'South Korea', 'CSE', 'BLɅϽKPIИK', '2021-9-1'),
-(5, 'Zhao Lu Si', 'China', 'CSE', 'BLɅϽKPIИK', '2021-9-1'),
+(5, 'Zhao Lu Si', 'China', 'CSE', 'Actress & Singer', '2021-9-1'),
 (6, 'Yuna', 'South Korea', 'CSE', 'ITZY', '2021-9-1'),
 (7, 'Ningning', 'China', 'CSE', 'aespa', '2021-9-1'),
 (8, 'Karina', 'South Korea', 'CSE', 'aespa', '2021-9-1'),
@@ -23,4 +23,13 @@ insert into Student_marks
 
 
        values
+    ('0xKingshuk', 100, 100, 100),
+    ('IVE', 100, 100, 100),
+    ('BLɅϽKPIИK', 100, 100, 100),
+    ('BLɅϽKPIИK', 100, 100, 100),
+    ('0xKingshuk', 100, 100, 100),
+    ('0xKingshuk', 100, 100, 100),
+    ('0xKingshuk', 100, 100, 100),
+    ('0xKingshuk', 100, 100, 100),
+    ('0xKingshuk', 100, 100, 100),
     ('0xKingshuk', 100, 100, 100),
