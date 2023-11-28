9b7b0379a1727f3164cad61505a6deb8520ae7bc
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-29 02:29:03 +0530

    updated

---
+++ DBMS/Assignment1-b.sql
@@ -0,0 +1,20 @@
+-- b)Insert 15 tuples of data in each table.
+
+insert into Student_details
+       (Student_ID, Name, Address, Branch, Roll_No, Admission_Date)
+values
+(1, 'ImKKingshuk', 'Metaverse, 127.0.0.1', 'CSE', '0xKingshuk', '2021-9-1'),
+(1, 'Wonyoung', 'South Korea', 'CSE', 'IVE', '2021-9-1'),
+(1, 'Rose', 'South Korea', 'CSE', 'BLɅϽKPIИK', '2021-9-1'),
+(1, 'Jennie', 'South Korea', 'CSE', 'BLɅϽKPIИK', '2021-9-1'),
+(1, '', '', 'CSE', '', '2021-9-1'),
+
+
+
+
+insert into Student_marks
+       (Roll_No, Subject_1, Subject_2, Subject_3)
+
+
+       values
+    ('20211022', 100, 100, 100),
