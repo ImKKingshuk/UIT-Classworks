d6e0835ec79c6f596b966fef5f99d340272f79da
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-29 03:10:59 +0530

    updated

--- DBMS/Assignment1-b.sql
+++ DBMS/Assignment1-b.sql
@@ -4,15 +4,6 @@ insert into Student_details
        (Student_ID, Name, Address, Branch, Roll_No, Admission_Date)
 values
 (1, 'ImKKingshuk', 'Metaverse, 127.0.0.1', 'CSE', '0xKingshuk', '2021-9-1'),
-(2, 'Wonyoung', 'South Korea', 'CSE', 'IVE', '2021-9-1'),
-(3, 'Rose', 'New Zealand', 'CSE', 'BLɅϽKPIИK', '2021-9-1'),
-(4, 'Jennie', 'South Korea', 'CSE', 'BLɅϽKPIИK', '2021-9-1'),
-(5, 'Zhao Lu Si', 'China', 'CSE', 'Actress & Singer', '2021-9-1'),
-(6, 'Yuna', 'South Korea', 'CSE', 'ITZY', '2021-9-1'),
-(7, 'Ningning', 'China', 'CSE', 'aespa', '2021-9-1'),
-(8, 'Karina', 'South Korea', 'CSE', 'aespa', '2021-9-1'),
-(9, 'Nova', 'Russia', 'CSE', 'X:IN', '2021-9-1'),
-(10, 'Nancy', 'United States of America', 'CSE', 'MOMOLAND', '2021-9-1'),
 
 
 
@@ -24,12 +15,4 @@ insert into Student_marks
 
        values
     ('0xKingshuk', 100, 100, 100),
-    ('IVE', 100, 100, 100),
-    ('BLɅϽKPIИK', 100, 100, 100),
-    ('BLɅϽKPIИK', 100, 100, 100),
-    ('0xKingshuk', 100, 100, 100),
-    ('0xKingshuk', 100, 100, 100),
-    ('0xKingshuk', 100, 100, 100),
-    ('0xKingshuk', 100, 100, 100),
-    ('0xKingshuk', 100, 100, 100),
-    ('0xKingshuk', 100, 100, 100),
+   
