1d8c15d5fdf51dce806d20a8ccffedeae430477b
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-29 03:03:03 +0530

    updated

--- DBMS/Assignment1-c.sql
+++ DBMS/Assignment1-c.sql
@@ -10,3 +10,11 @@ where (Name like 'S%' or Name like '%S');
 -- iii.Display students' information of 'CSE' and 'MCSE' departments.
 select * from Student_details
 where Branch = 'CSE' or Branch = 'MCSE';
+
+-- iv. Show the students' information of 'MCSE' department whose address is either 'Burdwan' or 'Durgapur'.
+select * from Student_details
+where Address = 'Burdwan' or Address = 'Durgapur';
+
+-- v.Show the Names & Student IDs of all students who got admission on and after 17-08-2021.
+select Name, Student_ID from Student_details
+where Admission_Date >= '2021-08-17';
