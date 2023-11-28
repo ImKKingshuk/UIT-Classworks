d26c0fd4d7eaa20aa1f83c63888761936e0796f3
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-29 02:38:56 +0530

    updated

--- DBMS/Assignment1-b.sql
+++ DBMS/Assignment1-b.sql
@@ -4,10 +4,15 @@ insert into Student_details
        (Student_ID, Name, Address, Branch, Roll_No, Admission_Date)
 values
 (1, 'ImKKingshuk', 'Metaverse, 127.0.0.1', 'CSE', '0xKingshuk', '2021-9-1'),
-(1, 'Wonyoung', 'South Korea', 'CSE', 'IVE', '2021-9-1'),
-(1, 'Rose', 'South Korea', 'CSE', 'BLɅϽKPIИK', '2021-9-1'),
-(1, 'Jennie', 'South Korea', 'CSE', 'BLɅϽKPIИK', '2021-9-1'),
-(1, '', '', 'CSE', '', '2021-9-1'),
+(2, 'Wonyoung', 'South Korea', 'CSE', 'IVE', '2021-9-1'),
+(3, 'Rose', 'New Zealand', 'CSE', 'BLɅϽKPIИK', '2021-9-1'),
+(4, 'Jennie', 'South Korea', 'CSE', 'BLɅϽKPIИK', '2021-9-1'),
+(5, 'Yuna', 'South Korea', 'CSE', 'ITZY', '2021-9-1'),
+(6, 'Ningning', 'China', 'CSE', 'aespa', '2021-9-1'),
+(7, 'Karina', 'South Korea', 'CSE', 'aespa', '2021-9-1'),
+(8, 'Nova', 'Russia', 'CSE', 'X:IN', '2021-9-1'),
+(9, 'Yuna', 'South Korea', 'CSE', '', '2021-9-1'),
+(10, 'Yuna', 'South Korea', 'CSE', '', '2021-9-1'),
 
 
 
