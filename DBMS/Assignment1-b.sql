c7a635ea9bce63119916104a801fece7e70c268d
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-29 03:50:30 +0530

    updated

--- DBMS/Assignment1-b.sql
+++ DBMS/Assignment1-b.sql
@@ -4,6 +4,12 @@ insert into Student_details
        (Student_ID, Name, Address, Branch, Roll_No, Admission_Date)
 values
 (1, 'ImKKingshuk', 'Metaverse, 127.0.0.1', 'CSE', '0xKingshuk', '2021-9-1'),
+(1, 'Scarlet Witch', 'Avengers', 'CSE', '0xScarletWitch', '2021-9-1'),
+(1, 'Captain Marvel', 'Avengers', 'CSE', '0xCaptainMarvel', '2021-9-1'),
+(1, 'Thor', 'Avengers', 'CSE', '0xThor', '2021-9-1'),
+(1, 'Dr. Strange', 'Avengers', 'CSE', '0xDr.Strange', '2021-9-1'),
+(1, 'Hulk', 'Avengers', 'CSE', '0xHulk', '2021-9-1'),
+(1, 'Iron Man', 'Avengers', 'CSE', '0xIronMan', '2021-9-1'),
 
 
 
