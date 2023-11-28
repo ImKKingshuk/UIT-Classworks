2ec5504453ca274950345da22ff98906d4140f31
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-29 03:46:09 +0530

    updated

--- DBMS/Assignment2-a.sql
+++ DBMS/Assignment2-a.sql
@@ -22,20 +22,14 @@ create table DEPT
 insert into EMP
        (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
 values
-(7369, 'Smith', 'Clerk', 7902, '1980-12-17', 800, null, 20),
-(7499, 'Allen', 'Salesman', 7698, '1981-02-20', 1600, 300, 30),
-(7521, 'Ward', 'Salesman', 7698, '1981-02-20', 1600, 300, 30),
-(7566, 'Jones', 'Manager', 7839, '1981-04-02', 2975, null, 20),
-(7654, 'Martin', 'Salesman', 7698, '1981-09-28', 1250, 1400, 30),
-(7689, 'Blake', 'Manager', 7839, '1981-05-01', 2850, null, 30),
-(7782, 'Clark', 'Manager', 7839, '1981-06-09', 2450, null, 10),
-(7788, 'Scott', 'Analyst', 7566, '1982-12-09', 3000, null, 20),
-(7839, 'King', 'President', null, '1981-11-17', 5000, null, 10),
-(7844, 'Turner', 'Salesman', 7698, '1981-09-08', 1500, 0, 30),
-(7876, 'Adams', 'Clerk', 7788, '1983-01-12', 1100, null, 20),
-(7900, 'James', 'Clerk', 7698, '1983-01-12', 1100, null, 20),
-(7788, 'Scott', 'Analyst', 7566, '1981-12-03', 950, null, 30),
-(7934, 'Miller', 'Clerk', 7782, '1982-01-23', 1300, null, 10);
+(7369, 'Thor', 'Clerk', 7902, '1980-12-17', 800, null, 20),
+(7499, 'Captain Marvel', 'Salesman', 7698, '1981-02-20', 1600, 300, 30),
+(7521, 'Scarlet Witch', 'Salesman', 7698, '1981-02-20', 1600, 300, 30),
+(7566, 'Dr. Strange', 'Manager', 7839, '1981-04-02', 2975, null, 20),
+(7654, 'Hulk', 'Salesman', 7698, '1981-09-28', 1250, 1400, 30),
+(7657, 'Iron Man', 'Salesman', 7698, '1981-09-28', 1250, 1400, 30),
+
+
 insert into DEPT
        (DEPTNO, DNAME, LOC)
   values 
