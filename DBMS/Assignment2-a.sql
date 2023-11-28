03e677a0030a87553191b854f242c582a47f0e1b
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-29 03:33:13 +0530

    updated

--- DBMS/Assignment2-a.sql
+++ DBMS/Assignment2-a.sql
@@ -1,21 +1,51 @@
--- a) Create the following tables:
+-- Create a database called COMPANY consisting of two tables - EMP & DEPT
 
--- Table Name: Student_details
-create table Student_details
-(
-Student_ID int primary key,
-Name varchar(25),
-Address varchar(35),
-Branch char(5) CHECK ("Branch" IN ('CSE', 'IT', 'ECE', 'AEIE', 'CE', 'EE', 'MCSE', 'MEIE')), Roll_No varchar(10) unique,
-       Admission_Date date check (Admission_Date < '2023-06-01')
 
--- Table Name: Student_marks
-create table Student_marks
-(
-);
-)
-Roll_No varchar(10) foreign key references Student_details(Roll_No),
-Subject_1 int check
-Subject_2 int check
-Subject_3 int check
-(Subject_1 >= 0 AND Subject_1 <= 100), (Subject_2 >= 0 AND Subject_2 <= 100), (Subject_3 >= 0 AND Subject_3 <= 100)
+create database COMPANY
+create table EMP
+ (
+ EMPNO int comment 'Employee number',
+ ENAME varchar comment 'Employee name',
+ JOB char comment 'Designation',
+ MGR int comment 'Manager Emp. number',
+ HIREDATE date comment 'Date of joining',
+ SAL int comment 'Basic salary',
+ COMM int comment 'Commission',
+ DEPTNO int comment 'Department number'
+ );
+create table DEPT
+ (
+ DEPTNO int comment 'Department number',
+ DNAME varchar(10) comment 'Department name',
+ LOC varchar(10) comment 'Location of department'
+ );
+insert into EMP
+       (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
+values
+(7369, 'Smith', 'Clerk', 7902, '1980-12-17', 800, null, 20),
+(7499, 'Allen', 'Salesman', 7698, '1981-02-20', 1600, 300, 30),
+(7521, 'Ward', 'Salesman', 7698, '1981-02-20', 1600, 300, 30),
+(7566, 'Jones', 'Manager', 7839, '1981-04-02', 2975, null, 20),
+(7654, 'Martin', 'Salesman', 7698, '1981-09-28', 1250, 1400, 30),
+(7689, 'Blake', 'Manager', 7839, '1981-05-01', 2850, null, 30),
+(7782, 'Clark', 'Manager', 7839, '1981-06-09', 2450, null, 10),
+(7788, 'Scott', 'Analyst', 7566, '1982-12-09', 3000, null, 20),
+(7839, 'King', 'President', null, '1981-11-17', 5000, null, 10),
+(7844, 'Turner', 'Salesman', 7698, '1981-09-08', 1500, 0, 30),
+(7876, 'Adams', 'Clerk', 7788, '1983-01-12', 1100, null, 20),
+(7900, 'James', 'Clerk', 7698, '1983-01-12', 1100, null, 20),
+(7788, 'Scott', 'Analyst', 7566, '1981-12-03', 950, null, 30),
+(7934, 'Miller', 'Clerk', 7782, '1982-01-23', 1300, null, 10);
+insert into DEPT
+       (DEPTNO, DNAME, LOC)
+  values 
+    (10, 'Accounting', 'New York'),
+    (20, 'Research', 'Dallas'),
+    (30, 'Sales', 'Chicago'),
+    (40, 'Operations', 'Boston');
+
+
+-- i.List the names of analysts and salesman.
+select ENAME, JOB from EMP
+where JOB in ('Analyst', 'Salesman');
+
