7c16e73bfaaa9ec4fd507f1a5eb511a1e40cbe51
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2024-06-17 17:59:57 +0530

    updated

---
+++ Python/prog7.py
@@ -0,0 +1,6 @@
+# Write a program to input a decimal number and prints out the decimal part of the number 
+
+number = float(input("Enter a decimal number: "))
+decimal_part = number - int(number)
+
+print(decimal_part)
