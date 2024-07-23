96f25b0eacb68d3d47c94e7b4a0c5cca80473277
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2024-07-24 02:15:29 +0530

    updated

--- 3rd Year/Python/test.py
+++ 3rd Year/Python/test.py
@@ -16,8 +16,67 @@ power=2**3
 print(f"power result : {power}")
 
 phone_brands = ["Apple", "Huawei", "Samsung", "Xiaomi"]
-print(f"Printing brands: {phone_brands[0]}")
+
+print(f"Printing brand 4: {phone_brands[3]} ")
+
+phone_brands.append("Meizu")
+
+print(f"Printing brands: {phone_brands} ")
+
+phone_brands.remove("Samsung")
+
+print(f"Printing brands: {phone_brands} ")
+
+
+phone_brand = ["Apple", "Huawei", "Samsung", "Xiaomi"]
+
+print(f"Printing brand 4: {phone_brands[3]} ")
+
+phone_brand.append("Meizu")
+
+print(f"Printing brands after appending 'Meizu': {phone_brand} ")
+
+# Remove multiple items using list comprehension
+phone_brand = [brand for brand in phone_brands if brand not in {"Samsung", "Apple"}]
+
+print(f"Printing brands after removing 'Samsung' and 'Apple': {phone_brand}")
+
+print( "test test test")
+
+# For loop
+for phone_brand in phone_brands:
+    print(phone_brand)
+
+# While loop
+count = 0
+while count < 3:
+    print(count)
+    count += 2
+
+
+age = 18
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
 
 
 
 
+# Creating a dictionary
+student = {
+    "name": "Alice",
+    "age": 25,
+    "is_student": True
+}
+
+# Accessing elements
+print(f"Here is student data: {student['name']}")  # Output: Alice
+
+
+# Adding elements
+student["grade"] = "A"
+
+# Removing elements
+del student["age"]
